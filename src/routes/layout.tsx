fb46132e65f3ebf1242cc1c224fa47d374f657e2
pnxbill <pnxbill@gmail.com>
2023-07-05 21:18:04 -0300

    feat: add logout functionality to layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,16 +1,27 @@
 import { $, component$, Slot, useContext } from '@builder.io/qwik';
-import Loader from '~/components/Loader';
 import SignIn from '~/components/SignIn';
 import Splash from '~/components/Splash';
-import { User, UserContext } from '~/context';
+import { AuthContext, User, UserContext } from '~/context';
 
 export default component$(() => {
   const { loading, id } = useContext<User>(UserContext);
+  const { logout } = useContext(AuthContext);
 
   const render = () => {
     if (loading) return <Splash />;
 
-    return <section>{id ? <Slot /> : <SignIn />}</section>;
+    return (
+      <section>
+        {id ? (
+          <>
+            <button onClick$={logout}>logout</button>
+            <Slot />{' '}
+          </>
+        ) : (
+          <SignIn />
+        )}
+      </section>
+    );
   };
 
   return <main>{render()}</main>;
